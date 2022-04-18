b57ad3f02afebc5b0eee20d2f7a7557e5dc27503
descriptor83 <descriptor83@yandex.ru>
2022-04-19 00:38:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const [{ Server: h1 }, x] = [require('http'), require('express')];
-
-const Router = x.Router();
-const PORT = 4321;
-const { log } = console;
-const hu = { 'Content-Type': 'text/plain; charset=utf-8' };
-const cors = { 'Access-Control-Allow-Origin' : '*' };
-const app = x();
-Router
-  .route('/')
-  .get(r => r.res.end('Привет мир!'));
-app
-  .use((r, rs, n) => rs.status(200).set(hu).set(cors) && n())
-  .use(x.static('.'))
-  .use('/', Router)
-  .get('/login/', (req, res, next) => {
-    if(req.query.error == 'yes') return next();
-    res.status(200);
-    res.send("descriptor83");
-  })
-  .get('/sample/', (req, res, next) => {
-    if(req.query.error == 'yes') return next();
-    res.status(200);
-    res.send("function task(x){ return x*this*this }");
-  })
-  .use(({ res: r }) => r.status(404).set(hu).send('Пока нет!'))
-  .use((e, r, rs, n) => rs.status(500).set(hu).send(`Ошибка: ${e}`))
-  /* .set('view engine', 'pug') */
-  .set('x-powered-by', false);
-module.exports = h1(app)
-  .listen(process.env.PORT || PORT, () => log(process.pid));
+const http = require('http'),
+PORT = 4321,
+headers = {
+  'Content-Type' : 'text/plain; charset=utf-8',
+  'Access-Control-Allow-Origin' : '*',
+  'Access-Control-Allow-Methods' : 'GET, POST',
+  'Access-Control-Allow-Headers' : 'Content-Type, Access-Control-Allow-Headers'
+}
+http.Server((req, res) =>{
+  if(req.url === '/'){
+      res.writeHead(200, headers) ;
+      res.end('Hello, World!');
+  }
+  else if(req.url === '/login/' ){
+    res.writeHead(200, headers);
+    res.end('descriptor83');
+  }
+   else if(req.url === '/sample/' ){
+    res.writeHead(200, headers);
+    res.end('function task(x){ return x*this*this; }');
+    
+  } 
+   else {
+    res.writeHead(200, headers); 
+    res.end('No');
+  }}).listen(process.env.PORT || PORT);
 
 
 /*
